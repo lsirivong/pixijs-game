59e35d8409caa31e38bb0d50cb29f05dacffb3a2
Lenny Sirivong <lenny@lenny-s.com>
2017-08-05 19:07:11 -0400

    2 jump sizes

--- game.js
+++ game.js
@@ -2,9 +2,9 @@ const PIXI = require('pixi.js');
 const _ = require('lodash');
 const { Vector2 } = require('three');
 
-const options = {
-  width: 256,
-  height: 256,
+const sizes = {
+  container: [256, 256],
+  player: [16, 16]
 }
 
 const KEY_LEFT = 37;
@@ -13,9 +13,12 @@ const KEY_UP = 38;
 const KEY_DOWN = 40;
 const KEY_SHIFT = 16;
 const KEY_SPACE = 32;
-const GRAVITY_A = 1.2;
-const JUMP_A = -15;
+const GRAVITY_A = 0.6;
+const GRAVITY_HEAVY_A = GRAVITY_A * 2;
+const JUMP_A = -10;
 const JUMP_V = new Vector2(0, -4);
+
+const LARGE_JUMP_THRESHOLD = 120;
 
 // Global state
 let _renderer = null;
@@ -23,11 +26,33 @@ let _stage = null;
 let _player = null;
 let _jumpAccel = 0;
 let _jumpT = null;
+let _jumpTimeout = null;
 const _playerVector = new Vector2(0, 0);
 const _keyState = [];
 
+const platforms = [
+  [ 20, 200, 80, 10 ]
+];
+
+/**
+ * returns a PIXI.Graphics object
+ */
+function renderRect(fill, x, y, width, height) {
+  const rectangle = new PIXI.Graphics();
+  rectangle.beginFill(fill);
+  rectangle.drawRect(0, 0, width, height);
+  rectangle.x = x;
+  rectangle.y = y;
+  rectangle.endFill();
+  return rectangle;
+}
 function handleKeyDownAction(keyCode) {
-  if (keyCode === KEY_SPACE && _jumpT === null) {
+  if (keyCode === KEY_SPACE && _player.y === sizes.container[1] - sizes.player[1]) {
+    _isLargeJump = null
+    _jumpTimeout = setTimeout(() => {
+      _jumpTimeout = null;
+      _isLargeJump = true;
+    }, LARGE_JUMP_THRESHOLD)
     _jumpT = 1;
   }
 }
@@ -41,6 +66,12 @@ function handleKeyDown({ keyCode }) {
 }
 
 function handleKeyUp({ keyCode }) {
+  if (keyCode === KEY_SPACE) {
+    if (_jumpTimeout) {
+      _isLargeJump = false;
+      clearTimeout(_jumpTimeout);
+    }
+  }
   const pos = _keyState.indexOf(keyCode);
   if (pos >= 0) {
     // remove it
@@ -50,7 +81,7 @@ function handleKeyUp({ keyCode }) {
 
 function init() {
   function initRenderer() {
-    const renderer = PIXI.autoDetectRenderer(options.width, options.height, {
+    const renderer = PIXI.autoDetectRenderer(sizes.container[0], sizes.container[0], {
     });
     renderer.view.classList.add('game-container');
     document.body.appendChild(renderer.view);
@@ -58,13 +89,13 @@ function init() {
   }
 
   function initPlayer() {
-    const rectangle = new PIXI.Graphics();
-    rectangle.beginFill(0xbaddad);
-    rectangle.drawRect(0, 0, 10, 10);
-    rectangle.y = options.height - 10;
-    rectangle.endFill();
+    return renderRect(0xbaddad, 0, sizes.container[1] - sizes.player[1], sizes.player[0], sizes.player[1]);
+  }
 
-    return rectangle;
+  function initPlatforms(platforms = []) {
+    return platforms.map(p => (
+      renderRect.apply(null, [0xd7c12d, ...p])
+    ));
   }
 
   function initStage(children) {
@@ -85,7 +116,8 @@ function init() {
   bindKeys();
   _renderer = initRenderer();
   _player = initPlayer();
-  _stage = initStage([ _player ]);
+  _platforms = initPlatforms(platforms);
+  _stage = initStage([ _player, ..._platforms ]);
 }
 
 function doAnimate() {
@@ -101,16 +133,16 @@ function doAnimate() {
 
   if (playerXVector !== 0) {
     const newX = _player.x + playerXVector;
-    _player.x = _.clamp(newX, 0, options.width - _player.width);
+    _player.x = _.clamp(newX, 0, sizes.container[0] - sizes.player[0]);
   }
 
   if (_jumpT !== null) {
-    const newVelocity = new Vector2(0, JUMP_A).add(new Vector2(0, _jumpT * GRAVITY_A));
+    const newVelocity = new Vector2(0, JUMP_A).add(new Vector2(0, _jumpT * (_isLargeJump === false ? GRAVITY_HEAVY_A : GRAVITY_A )));
     const newPlayerPos = new Vector2(_player.x, _player.y).add(newVelocity);
 
-    const MAX_Y = options.height - _player.height;
+    const MAX_Y = sizes.container[1] - sizes.player[1];
     // keep in bounds;
-    _player.x = _.clamp(newPlayerPos.x, 0, options.width - _player.width);
+    _player.x = _.clamp(newPlayerPos.x, 0, sizes.container[0] - sizes.player[0]);
     _player.y = _.clamp(newPlayerPos.y, 0, MAX_Y);
 
     _jumpT++;
